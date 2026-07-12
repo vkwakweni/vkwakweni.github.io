870eadbff5be3f360df0322d61cf0015df119bee
Vuyo Kwakweni <b2fkcj@tr.pte.hu>
2026-07-12 21:05:30 +0200

    added directory flag for encryption

--- encrypt_site.sh
+++ encrypt_site.sh
@@ -10,7 +10,8 @@ echo "Starting vkwakweni portfolio site encryption..."
 
 find ./build -name "*.html" | while read -r file; do
     echo "Encrypting: $file"
-    npx staticrypt "$file" -p "$PASSWORD" -o "$file" --short
+    npx staticrypt "$file" -p "$PASSWORD" -o "$file" --short -d "$(dirname "$file")"
+    mv "${file%.html}_encrypted.html" "$file" 2>/dev/null
 done
 
 echo "All HTML pages successfully encrypted!"
