3f55d8d76630a4236cf09e6645596b7e16c7d9c4
Vuyo Kwakweni <b2fkcj@tr.pte.hu>
2026-07-12 18:46:51 +0200

    updated encryption file

--- encrypt_site.sh
+++ encrypt_site.sh
@@ -8,9 +8,9 @@ echo "Starting vkwakweni portfolio site encryption..."
 
 # find every .html file in the build directory and encrypt it
 
-find build -name "*.html" | while read -r file; directory
+find build -name "*.html" | while read -r file; do
     echo "Encrypting: $file"
-    staticrypt "$file" "$PASSWORD" -o "$file"
+    npx staticrypt "$file" "$PASSWORD" -o "$file"
 done
 
-echo "All HTML pages successfull encrypted!"
+echo "All HTML pages successfully encrypted!"
